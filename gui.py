3a80ec86d5e0cde9e7a1774d790034e8bb649359
Sejal Jain <jain.sejal97@gmail.com>
2018-11-13 22:35:52 -0500

    added gui files

---
+++ gui.py
@@ -0,0 +1,185 @@
+from minichess.MinichessLogic import Board as board
+import tkinter as tk
+from PIL import Image, ImageTk
+
+
+abbrev = {
+ 4:'r',
+ 2:'n',
+ 3:'b',
+ 5:'q',
+ 6:'k',
+ 1:'p',
+ -4:'r',
+ -2:'n',
+ -3:'b',
+ -5:'q',
+ -6:'k',
+ -1:'p'
+}
+
+
+class BoardGuiTk(tk.Frame):
+    pieces = {}
+    selected = None
+    selected_piece = None
+    hilighted = None
+    icons = {}
+
+    color1 = "white"
+    color2 = "grey"
+
+    rows = 5
+    columns = 5
+
+    @property
+    def canvas_size(self):
+        return (self.columns * self.square_size,
+                self.rows * self.square_size)
+
+    def __init__(self, parent, chessboard, square_size=64):
+
+        self.chessboard = chessboard
+        self.square_size = square_size
+        self.parent = parent
+
+        canvas_width = self.columns * square_size
+        canvas_height = self.rows * square_size
+
+        tk.Frame.__init__(self, parent)
+
+        self.canvas = tk.Canvas(self, width=canvas_width, height=canvas_height, background="grey")
+        self.canvas.pack(side="top", fill="both", anchor="c", expand=True)
+        
+        self.canvas.bind("<Configure>", self.refresh)
+        self.canvas.bind("<Button-1>", self.click)
+        
+        self.statusbar = tk.Frame(self, height=64)
+        self.button_quit = tk.Button(self.statusbar, text="New", fg="black", command=self.reset)
+        self.button_quit.pack(side=tk.LEFT, in_=self.statusbar)
+
+        #self.button_save = tk.Button(self.statusbar, text="Save", fg="black", command=self.chessboard.save_to_file)
+        #self.button_save.pack(side=tk.LEFT, in_=self.statusbar)
+
+        self.label_status = tk.Label(self.statusbar, text="   White's turn  ", fg="black")
+        self.label_status.pack(side=tk.LEFT, expand=0, in_=self.statusbar)
+
+        self.button_quit = tk.Button(self.statusbar, text="Quit", fg="black", command=self.parent.destroy)
+        self.button_quit.pack(side=tk.RIGHT, in_=self.statusbar)
+        self.statusbar.pack(expand=False, fill="x", side='bottom')
+        
+
+    def click(self, event):
+
+        # Figure out which square we've clicked
+        col_size = row_size = event.widget.master.square_size
+
+        current_column = event.x / col_size
+        current_row = 7 - (event.y / row_size)
+
+        position = self.chessboard.letter_notation((current_row, current_column))
+        piece = self.chessboard[position]
+
+        if self.selected_piece:
+            self.move(self.selected_piece[1], position)
+            self.selected_piece = None
+            self.hilighted = None
+            self.pieces = {}
+            self.refresh()
+            self.draw_pieces()
+
+        self.hilight(position)
+        self.refresh()
+
+    def move(self, p1, p2):
+        piece = self.chessboard[p1]
+        dest_piece = self.chessboard[p2]
+        if dest_piece is None or dest_piece.color != piece.color:
+            try:
+                self.chessboard.move(p1,p2)
+            except board.ChessError as error:
+                self.label_status["text"] = error.__class__.__name__
+            else:
+                self.label_status["text"] = " " + piece.color.capitalize() +": "+ p1 + p2
+
+
+    def addpiece(self, name, image, row=0, column=0):
+        '''Add a piece to the playing board'''
+        self.canvas.create_image(0,0, image=image, tags=(name, "piece"), anchor="c")
+        self.placepiece(name, row, column)
+
+    def placepiece(self, name, row, column):
+        '''Place a piece at the given row/column'''
+        self.pieces[name] = (row, column)
+        x0 = (column * self.square_size) + int(self.square_size/2)
+        y0 = ((7-row) * self.square_size) + int(self.square_size/2)
+        self.canvas.coords(name, x0, y0)
+
+    def refresh(self, event={}):
+        '''Redraw the board'''
+        if event:
+            xsize = int((event.width-1) / self.columns)
+            ysize = int((event.height-1) / self.rows)
+            self.square_size = min(xsize, ysize)
+
+        self.canvas.delete("square")
+        color = self.color2
+        for row in range(self.rows):
+            color = self.color1 if color == self.color2 else self.color2
+            for col in range(self.columns):
+                x1 = (col * self.square_size)
+                y1 = ((7-row) * self.square_size)
+                x2 = x1 + self.square_size
+                y2 = y1 + self.square_size
+                if (self.selected is not None) and (row, col) == self.selected:
+                    self.canvas.create_rectangle(x1, y1, x2, y2, outline="black", fill="orange", tags="square")
+                elif(self.hilighted is not None and (row, col) in self.hilighted):
+                    self.canvas.create_rectangle(x1, y1, x2, y2, outline="black", fill="spring green", tags="square")
+                else:
+                    self.canvas.create_rectangle(x1, y1, x2, y2, outline="black", fill=color, tags="square")
+                color = self.color1 if color == self.color2 else self.color2
+        for name in self.pieces:
+            self.placepiece(name, self.pieces[name][0], self.pieces[name][1])
+        self.canvas.tag_raise("piece")
+        self.canvas.tag_lower("square")
+
+    def draw_pieces(self):
+        self.canvas.delete("piece")
+        for x in range(self.chessboard.dim[0]):
+            for y in range(self.chessboard.dim[1]):
+                p = self.chessboard.board[x][y]
+                if p != 0:
+                    if p > 0:
+                        filename = ("img/white%s.png" % (abbrev[p]))
+                        piecename = ("%s%s%s" % (abbrev[p], x, y))
+                    else:
+                        filename = ("img/black%s.png" % (abbrev[p]))
+                        piecename = ("%s%s%s" % (abbrev[p], x, y))
+
+                    if(filename not in self.icons):
+                        self.icons[filename] = ImageTk.PhotoImage(file=filename, width=32, height=32)
+                        
+                    self.addpiece(piecename, self.icons[filename], x, y)
+                    self.placepiece(piecename, x, y)
+
+
+    def reset(self):
+        self.chessboard.load(board.FEN_STARTING)
+        self.refresh()
+        self.draw_pieces()
+        self.refresh()
+
+def display(chessboard):
+    print(chessboard.board)
+    root = tk.Tk() #creates window
+    root.title("Simple Python Chess")
+
+    gui = BoardGuiTk(root, chessboard)
+    gui.pack(side="top", fill="both", expand="true", padx=4, pady=4)
+    gui.draw_pieces()
+
+    root.resizable(0,0)
+    root.mainloop()
+
+if __name__ == "__main__":
+    display()
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from Coach import Coach
 # from othello.OthelloGame import OthelloGame as Game
 from minichess.MinichessGame import MinichessGame as Game
+from minichess.MinichessLogic import Board
+
 from othello.pytorch.NNet import NNetWrapper as nn
 from utils import *
+import sys 
 
 args = dotdict({
     'numIters': 1000,
@@ -24,7 +27,24 @@ args = dotdict({
 if __name__ == "__main__":
     g = Game(5, 5)
     nnet = nn(g)
+    b = Board()
 
+    if len(sys.argv) > 1:
+        if sys.argv[1] in ('--console', '-c'):
+            from gui import display
+            display(b)
+            exit(0)
+        elif sys.argv[1] in ('--help', '-h'):
+            print ('''Usage: game.py [OPTION]\n\n\tPlay a game of chess\n\n\tOptions:\n\t -c, --console\tplay in console mode\n\n''')
+            exit(0)
+
+    try:
+        from gui import display
+    except ImportError:
+        from gui import display
+    finally:
+        display(b)
+'''
     if args.load_model:
         nnet.load_checkpoint(args.load_folder_file[0], args.load_folder_file[1])
 
@@ -33,3 +53,4 @@ if __name__ == "__main__":
         print("Load trainExamples from file")
         c.loadTrainExamples()
     c.learn()
+'''
