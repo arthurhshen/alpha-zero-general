53a89805e9ac1160ee6788430397936f6452ad78
Arthur Shen <ahs2164@columbia.edu>
2018-12-01 20:10:23 -0500

    Added 50-move rule to prevent infinite games

--- MCTS.py
+++ MCTS.py
@@ -5,6 +5,7 @@ import copy
 
 EPS = 1e-8
 
+
 class MCTS():
     """
     This class handles the MCTS tree.
@@ -37,20 +38,19 @@ class MCTS():
             self.game = tmp_game
 
         s = self.game.stringRepresentation(canonicalBoard)
-        counts = [self.Nsa[(s,a)] if (s,a) in self.Nsa else 0 for a in range(self.game.getActionSize())]
+        counts = [self.Nsa[(s, a)] if (s, a) in self.Nsa else 0 for a in range(self.game.getActionSize())]
 
-        if temp==0:
+        if temp == 0:
             bestA = np.argmax(counts)
-            probs = [0]*len(counts)
-            probs[bestA]=1
+            probs = [0] * len(counts)
+            probs[bestA] = 1
             return probs
 
-        counts = [x**(1./temp) for x in counts]
-        probs = [x/float(sum(counts)) for x in counts]
+        counts = [x**(1. / temp) for x in counts]
+        probs = [x / float(sum(counts)) for x in counts]
         return probs
 
-
-    def search(self, canonicalBoard):
+    def search(self, canonicalBoard, depth=0):
         """
         This function performs one iteration of MCTS. It is recursively called
         till a leaf node is found. The action chosen at each node is one that
@@ -70,10 +70,13 @@ class MCTS():
             v: the negative of the value of the current canonicalBoard
         """
 
+        if depth >= 100:
+            return 1e-8
+
         s = self.game.stringRepresentation(canonicalBoard)
 
         game_end_score = self.game.getGameEnded(canonicalBoard, 1)
-        if game_end_score !=0:
+        if game_end_score != 0:
             return -game_end_score
 
         # if s not in self.Es:
@@ -86,7 +89,7 @@ class MCTS():
             # leaf node
             self.Ps[s], v = self.nnet.predict(canonicalBoard)
             valids = self.game.getValidMoves(canonicalBoard, 1)
-            self.Ps[s] = self.Ps[s]*valids      # masking invalid moves
+            self.Ps[s] = self.Ps[s] * valids      # masking invalid moves
             sum_Ps_s = np.sum(self.Ps[s])
             if sum_Ps_s > 0:
                 self.Ps[s] /= sum_Ps_s    # renormalize
@@ -110,10 +113,10 @@ class MCTS():
         # pick the action with the highest upper confidence bound
         for a in range(self.game.getActionSize()):
             if valids[a]:
-                if (s,a) in self.Qsa:
-                    u = self.Qsa[(s,a)] + self.args.cpuct*self.Ps[s][a]*math.sqrt(self.Ns[s])/(1+self.Nsa[(s,a)])
+                if (s, a) in self.Qsa:
+                    u = self.Qsa[(s, a)] + self.args.cpuct * self.Ps[s][a] * math.sqrt(self.Ns[s]) / (1 + self.Nsa[(s, a)])
                 else:
-                    u = self.args.cpuct*self.Ps[s][a]*math.sqrt(self.Ns[s] + EPS)     # Q = 0 ?
+                    u = self.args.cpuct * self.Ps[s][a] * math.sqrt(self.Ns[s] + EPS)     # Q = 0 ?
 
                 if u > cur_best:
                     cur_best = u
@@ -123,18 +126,33 @@ class MCTS():
         next_s, next_player = self.game.getNextState(canonicalBoard, 1, a)
         next_s = self.game.getCanonicalForm(next_s, next_player)
 
+        # Check to see if a pawn was moved, or if a piece was captured
+
+        start_square, end_square = self.game.index_dict[a]
+
+        # print("Action: ", a)
+        if abs(canonicalBoard[start_square[0]][start_square[1]]) == 1:
+            # print(abs(canonicalBoard[start_square[0]][start_square[1]]))
+            depth = 0
+        elif canonicalBoard[end_square[0]][end_square[1]] != 0:
+            # print(canonicalBoard[end_square[0]][end_square[1]])
+            depth = 0
+        else:
+            depth = depth + 1
+
         try:
-            v = self.search(next_s)
+            v = self.search(next_s, depth)
         except:
+            print("Exception in MCTS - search")
             return 1e-8
-            
-        if (s,a) in self.Qsa:
-            self.Qsa[(s,a)] = (self.Nsa[(s,a)]*self.Qsa[(s,a)] + v)/(self.Nsa[(s,a)]+1)
-            self.Nsa[(s,a)] += 1
+
+        if (s, a) in self.Qsa:
+            self.Qsa[(s, a)] = (self.Nsa[(s, a)] * self.Qsa[(s, a)] + v) / (self.Nsa[(s, a)] + 1)
+            self.Nsa[(s, a)] += 1
 
         else:
-            self.Qsa[(s,a)] = v
-            self.Nsa[(s,a)] = 1
+            self.Qsa[(s, a)] = v
+            self.Nsa[(s, a)] = 1
 
         self.Ns[s] += 1
         return -v
