7ce7124eaf5a30f2c0abb89f3a4bd2f0a4988f94
Arthur Shen <ahs2164@columbia.edu>
2018-12-08 18:51:42 -0500

    Fixed typo in NNet.py

--- pit_minichess.py
+++ pit_minichess.py
@@ -1,6 +1,6 @@
 import Arena
 from MCTS import MCTS
-from minichess.MinichessGame import MinichessGame, display
+from minichess.MinichessGame import MinichessGame
 from minichess.MinichessMinimax import *
 from minichess.NNetPlayer import *
 from minichess.keras.NNet import NNetWrapper as NNet
@@ -19,10 +19,14 @@ minimax_p1 = MinimaxPlayer(g, 1).select_move
 
 # nnet players
 n1 = NNet(g)
-n1.load_checkpoint('./temp/','temp.pth.tar')
-args1 = dotdict({'numMCTSSims': 50, 'cpuct':1.0})
+n1.load_checkpoint('./temp/', 'temp.pth.tar')
+args1 = dotdict({'numMCTSSims': 50, 'cpuct': 1.0})
 mcts1 = MCTS(g, n1, args1)
-n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
+
+
+def n1p(x):
+    return np.argmax(mcts1.getActionProb(x, temp=0))
+
 
 arena = Arena.Arena(n1p, minimax_p1, g, display=g.display)
 print(arena.playGames(10, verbose=True))
